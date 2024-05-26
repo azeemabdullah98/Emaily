5466ffdbb5e19313f7824d482a65e26693b6d1a7
azeemabdullah98 <azeemquest98@gmail.com>
2024-05-26 10:17:31 +0530

    changed the response

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.send("Hello Azeem!");
 });
 
 const PORT = process.env.PORT || 3000;
