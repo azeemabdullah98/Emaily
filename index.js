877a1781b0f6d9551c64f78cd3b412cfcec4df34
azeemabdullah98 <azeemquest98@gmail.com>
2024-05-30 09:46:33 +0530

    separate dev from prod

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-import express from "express";
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
+
+mongoose
+  .connect(keys.mongoURI)
+  .then(() => {
+    console.log("connected to mongoDB");
+  })
+  .catch((err) => console.error("could not connect to mongoDB", err));
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello Azeem!");
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 3000;
 
